ab3f56d1a7998d45c0ed54e21dffb398ac16603b
ryojimizuno <47686078+ryojimizuno@users.noreply.github.com>
2019-02-16 16:56:49 +0900

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="<?php bloginfo( 'charset' ); //文字エンコーディング情報を出力 ?>">
     <title><?php wp_title( '|', true, 'right' ); //ページタイトルを出力 ?><?php bloginfo('name'); //サイト名を表示 ?></title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); //使用中テーマディレクトリのURLを出力 ?>/css/normalize.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); //使用中テーマディレクトリのURLを出力 ?>/normalize.css">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); //使用中テーマのスタイルシートURLを出力 ?>">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); //ピングバックURLを出力 ?>">
     <?php wp_head(); //wp_headはテーマの</head>タグ直前に必ず挿入します ?>
